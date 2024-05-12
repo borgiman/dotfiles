c200d73a71eabf4419ba4dfd440015ea7dd7240d
Patrick Borgogno <789401+borgiman@users.noreply.github.com>
2024-05-12 11:43:53 +0200

    add post install steps

--- install.sh
+++ install.sh
@@ -99,3 +99,7 @@ defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Google Chrome.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/System/Applications/Utilities/Terminal.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 killall Dock
+
+echo '*** Finished, do these steps now manually'
+echo '-> Log in to Microsoft Teams'
+echo '-> Log in to Google Chrome'
